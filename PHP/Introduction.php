d198aa9da583439cdfbb2933e9dbb2154c8c130b
Tetris <truongvietduc@gmail.com>
2019-06-13 14:41:44 +0700

    Introduction file created

---
+++ PHP/Introduction.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Introduction</title>
+    </head>
+    <body>
+        <?php
+        echo "Hello <br>";
+        echo "<p> <font color=teal font face='courier'>Name: Duc <br>";
+        echo "<p> <font color=teal font face='courier'>Major: IT <br>";
+        ?>
+    </body>
+</html>
